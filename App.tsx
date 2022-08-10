052561f98c17bee5820a1691dfa1e92ccd80c0d4
Tasman Bruce <tasmanbrucework@gmail.com>
2022-08-10 23:32:12 +1200

    Added ignore for AsyncStorage warning. This is a result of Firebase and cannot be fixed.

--- App.tsx
+++ App.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import { NativeBaseProvider } from 'native-base';
+import { LogBox } from 'react-native';
 import useCachedResources from './hooks/useCachedResources';
 import AppNavigator from './navigators/AppNavigator';
 import theme from './theme';
+
+LogBox.ignoreLogs(['AsyncStorage has been extracted']);
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
