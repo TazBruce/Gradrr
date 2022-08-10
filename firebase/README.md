335ba24f8070bc9dc35c34a4e771509eb1f2e3b3
Tasman Bruce <tasmanbrucework@gmail.com>
2022-08-11 01:23:37 +1200

    Implemented firebase function for automatically creating user documents

---
+++ firebase/README.md
@@ -0,0 +1,11 @@
+# Firebase
+
+For managing cloud functions and firestore
+
+## Usage
+
+To deploy the source code to firebase, use:
+
+```bash
+firebase deploy
+```
--- firebase/functions/src/index.ts
+++ firebase/functions/src/index.ts
@@ -1,9 +1,20 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = firebase.https.onRequest((request, response) => {
-//   firebase.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+admin.initializeApp();
+const firestore = admin.firestore();
+
+export const authOnCreate =
+  functions.auth.user().onCreate(async (user) => {
+    console.log(`Creating document for user ${user.uid}`);
+    await firestore.collection('users').doc(user.uid).set({
+      createdAt: admin.firestore.FieldValue.serverTimestamp(),
+      credits: 5
+    });
+  });
+
+export const authOnDelete =
+  functions.auth.user().onDelete(async (user) => {
+    console.log(`Deleting document for user ${user.uid}`);
+    await firestore.collection('users').doc(user.uid).delete();
+  });
