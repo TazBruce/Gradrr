295f0cda348a02d1f15757bede3fa8a7fd636321
Tasman Bruce <tasmanbrucework@gmail.com>
2022-09-01 16:44:33 +1200

    Basic assignmentRow laid out

--- app/src/components/assignments/AssignmentRow.tsx
+++ app/src/components/assignments/AssignmentRow.tsx
@@ -1,8 +1,17 @@
 import React from "react";
-import { Heading, HStack } from "native-base";
+import {
+  Divider,
+  Heading,
+  HStack,
+  Icon,
+  IconButton,
+  Square,
+  VStack,
+} from "native-base";
 import { CourseStackParamList } from "../../types/Course";
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
 import { Assignment } from "../../types/Assignment";
+import { MaterialIcons } from "@expo/vector-icons";
 
 /**
  * Renders an assignment row.
@@ -16,9 +25,29 @@ export default function AssignmentRow(
   assignment: Assignment,
   key: React.Key | undefined
 ): JSX.Element {
+  const dueDate = assignment.due_date.toDate().toDateString();
   return (
     <HStack key={key} padding={3} space={4} justifyContent="space-evenly">
-      <Heading>{assignment.title}</Heading>
+      <Square size="xs" bg="primary.500" rounded="md" shadow={3} />
+      <VStack>
+        <Heading>{assignment.title}</Heading>
+        <Heading>{dueDate.substring(0, dueDate.length - 4)}</Heading>
+      </VStack>
+      <Divider bg="coolGray.500" orientation="vertical" mx={4} thickness="2" />
+      <VStack>
+        <Heading>Weight</Heading>
+        <Heading>25%</Heading>
+      </VStack>
+      <IconButton
+        icon={
+          <Icon
+            size="xl"
+            color="black"
+            as={<MaterialIcons name="keyboard-arrow-right" />}
+          />
+        }
+        onPress={() => console.log("Assignment pressed")}
+      />
     </HStack>
   );
 }
