c6bbf9ead78a3801c09075e7d9180fe399a74416
Tasman Bruce <tasmanbrucework@gmail.com>
2022-09-06 01:12:14 +1200

    Implemented centering, pressable element

--- app/src/components/courses/CourseRow.tsx
+++ app/src/components/courses/CourseRow.tsx
@@ -7,6 +7,7 @@ import {
   VStack,
   Text,
   Square,
+  Pressable,
 } from "native-base";
 import { Course, CourseStackParamList } from "../../types/Course";
 import { MaterialIcons } from "@expo/vector-icons";
@@ -34,34 +35,40 @@ export default function CourseRow(
   course: Course
 ): JSX.Element {
   return (
-    <HStack
+    <Pressable
       key={course.id}
-      paddingTop={3}
-      paddingBottom={2}
-      space={4}
-      w="100%"
-      justifyContent="flex-start"
+      onPress={() => navigation.navigate("ViewCourse", { course: course })}
     >
-      <Square size="md" bg="primary.500" rounded="md" shadow={3} w="15%" />
-      <VStack paddingTop={2} alignContent="flex-start" w="45%">
-        <Heading size="md">{course.title}</Heading>
-        <Text numberOfLines={2}>{course.description}</Text>
-      </VStack>
-      <Heading alignSelf="center" size="2xl" alignContent="flex-end" w="20%">
-        {getGrade(course)}
-      </Heading>
-      <IconButton
-        icon={
-          <Icon
-            size="xl"
-            color="black"
-            as={<MaterialIcons name="keyboard-arrow-right" />}
-          />
-        }
-        onPress={() => navigation.navigate("ViewCourse", { course: course })}
-        w="5%"
-        alignContent="flex-end"
-      />
-    </HStack>
+      <HStack
+        paddingTop={3}
+        paddingBottom={2}
+        space={4}
+        justifyContent="flex-start"
+      >
+        <Square
+          size="md"
+          bg="primary.500"
+          rounded="md"
+          shadow={3}
+          w="15%"
+          alignSelf="center"
+        />
+        <VStack paddingTop={2} alignContent="flex-start" w="45%">
+          <Heading size="md">{course.title}</Heading>
+          <Text numberOfLines={2}>{course.description}</Text>
+        </VStack>
+        <Heading alignSelf="center" size="2xl" alignContent="flex-end" w="20%">
+          {getGrade(course)}
+        </Heading>
+        <Icon
+          size="xl"
+          color="black"
+          as={<MaterialIcons name="keyboard-arrow-right" />}
+          w="5%"
+          alignSelf="center"
+          alignContent="flex-end"
+        />
+      </HStack>
+    </Pressable>
   );
 }
