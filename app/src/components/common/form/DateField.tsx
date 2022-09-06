83ab74d73b792a9766b36afddf93eb4875b4237e
Tasman Bruce <tasmanbrucework@gmail.com>
2022-09-06 23:11:53 +1200

    Basic datefield functionality

--- app/src/components/common/form/DateField.tsx
+++ app/src/components/common/form/DateField.tsx
@@ -1,8 +1,9 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import React, { useState } from "react";
-import { IInputProps, FormControl, Button } from "native-base";
+import { IInputProps, FormControl, Input, IconButton, Icon } from "native-base";
 import { useField } from "formik";
 import RNDateTimePicker from "@react-native-community/datetimepicker";
+import { MaterialIcons } from "@expo/vector-icons";
 
 interface TextFieldProps extends IInputProps {
   name: string;
@@ -27,21 +28,57 @@ export default function DateField(props: TextFieldProps) {
   });
   const [show, setShow] = useState(false);
 
-  const onChange = (event: any, selectedDate: any) => {
+  const onChange = (event: any, selectedDate: Date | undefined) => {
     setShow(false);
-    setDate(selectedDate);
-    setValue(selectedDate);
+    if (selectedDate) {
+      console.log("Changed:");
+      setDate(selectedDate);
+      setValue(selectedDate);
+    }
   };
 
   const showDatepicker = () => {
     setShow(true);
   };
 
+  const handleClick = () => {
+    console.log("Clear date pressed");
+  };
+
   return (
     <FormControl isRequired={isRequired} isInvalid={Boolean(meta.error)}>
       <FormControl.Label>{label}</FormControl.Label>
-      <Button onPress={showDatepicker}>{date.toDateString()}</Button>
-      {show && <RNDateTimePicker value={date} onChange={onChange} />}
+      <Input
+        isDisabled={true}
+        value={date.toDateString()}
+        InputLeftElement={
+          <IconButton
+            icon={
+              <Icon
+                size="lg"
+                color="primary.500"
+                as={<MaterialIcons name="date-range" />}
+              />
+            }
+            onPress={showDatepicker}
+          ></IconButton>
+        }
+        InputRightElement={
+          <IconButton
+            icon={
+              <Icon
+                size="lg"
+                color="primary.400"
+                as={<MaterialIcons name="delete" />}
+              />
+            }
+            onPress={handleClick}
+          ></IconButton>
+        }
+      ></Input>
+      {show && (
+        <RNDateTimePicker display="spinner" value={date} onChange={onChange} />
+      )}
       {meta.error && (
         <FormControl.ErrorMessage>{meta.error}</FormControl.ErrorMessage>
       )}
