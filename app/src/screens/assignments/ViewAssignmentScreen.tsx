38d86c010433e5dcf9166b31af3b5e7e30736946
Tasman Bruce <tasmanbrucework@gmail.com>
2022-09-08 00:28:44 +1200

    Basic ViewAssignmentScreen.tsx layout

--- app/src/screens/assignments/ViewAssignmentScreen.tsx
+++ app/src/screens/assignments/ViewAssignmentScreen.tsx
@@ -1,9 +1,14 @@
 import React from "react";
-import { Heading, VStack } from "native-base";
+import { Button, Fab, Heading, Icon, VStack } from "native-base";
 import Screen from "../../components/common/Screen";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { CourseStackParamList } from "../../types/Course";
-import { getDueDate } from "../../types/Assignment";
+import {
+  getDueDate,
+  getWeight,
+  initialAssignment,
+} from "../../types/Assignment";
+import { AntDesign } from "@expo/vector-icons";
 
 export default function ViewAssignmentScreen({
   route,
@@ -20,7 +25,22 @@ export default function ViewAssignmentScreen({
       <VStack space="md">
         <Heading size="md">{assignment.description}</Heading>
         <Heading size="md">{getDueDate(assignment)}</Heading>
+        <Heading size="md">{getWeight(assignment)}</Heading>
+        <Heading size="md">Subtasks</Heading>
       </VStack>
+      <Button>Mark as Complete</Button>
+      <Fab
+        renderInPortal={false}
+        bottom={30}
+        shadow={2}
+        size="sm"
+        backgroundColor="primary.500"
+        _pressed={{ backgroundColor: "primary.600" }}
+        icon={<Icon color="white" as={<AntDesign name="plus" />} />}
+        onPress={() =>
+          console.log("Add subtask to assignment " + assignment.title)
+        }
+      />
     </Screen>
   );
 }
