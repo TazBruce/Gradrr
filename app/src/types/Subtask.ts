8a1c164bb773867673f50e595397d3d36b491cbb
Tasman Bruce <tasmanbrucework@gmail.com>
2022-09-08 00:43:00 +1200

    Added basic subtask Type

---
+++ app/src/types/Subtask.ts
@@ -0,0 +1,33 @@
+import { Key } from "react";
+import { Timestamp } from "firebase/firestore";
+
+export type Subtask = {
+  /**
+   * The id of the subtask
+   */
+  id?: Key;
+  /**
+   * The owner of the subtask
+   */
+  owner: string;
+  /**
+   * The name of the subtask
+   */
+  title: string;
+  /**
+   * The date the subtask is due
+   */
+  due_date: Timestamp | Date | null;
+  /**
+   * Whether the subtask has been completed
+   */
+  is_complete: boolean;
+};
+
+export const initialSubtask: Subtask = {
+  id: "",
+  owner: "",
+  title: "",
+  due_date: null,
+  is_complete: false,
+};
