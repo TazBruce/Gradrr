a014ce7e8e51819522d373a632b8c4575a705499
Tasman Bruce <tasmanbrucework@gmail.com>
2022-09-08 12:20:24 +1200

    Basic subtask row aesthetics

--- app/src/components/subtasks/SubtaskRow.tsx
+++ app/src/components/subtasks/SubtaskRow.tsx
@@ -1,5 +1,13 @@
 import React, { useContext, useState } from "react";
-import { Heading, HStack, Icon, IconButton, VStack } from "native-base";
+import {
+  Container,
+  Heading,
+  HStack,
+  Icon,
+  IconButton,
+  VStack,
+  Text,
+} from "native-base";
 import { MaterialIcons } from "@expo/vector-icons";
 import { AuthContext } from "../../providers/AuthProvider";
 import { collection, doc } from "firebase/firestore";
@@ -38,19 +46,28 @@ export default function SubtaskRow(props: RowProps): JSX.Element {
     });
   };
 
+  const deleteSubtask = () => {
+    console.log("Delete subtask");
+  };
+
+  const editSubtask = () => {
+    console.log("Edit subtask");
+  };
+
   return (
     <HStack
       key={subtask.id}
       paddingTop={3}
       paddingBottom={3}
       space={3}
-      h="60"
+      h="70"
       w="100%"
       justifyContent="flex-start"
     >
       <IconButton
         onPress={markComplete}
         bg={completed ? "primary.500" : "white"}
+        margin={2}
         rounded="md"
         borderColor={completed ? "primary.500" : "coolGray.500"}
         borderWidth={4}
@@ -64,8 +81,34 @@ export default function SubtaskRow(props: RowProps): JSX.Element {
         <Heading size="md" numberOfLines={1}>
           {subtask.title}
         </Heading>
-        <Heading size="md">{getDueDate(subtask)}</Heading>
+        <HStack space={2}>
+          <Icon
+            as={MaterialIcons}
+            name="date-range"
+            size="md"
+            color={"primary.500"}
+          />
+          <Text>{getDueDate(subtask)}</Text>
+        </HStack>
       </VStack>
+      <IconButton
+        onPress={editSubtask}
+        bg="white"
+        shadow={3}
+        h="100%"
+        w="15%"
+        alignContent="flex-end"
+        icon={<Icon as={MaterialIcons} name="edit" color="primary.500" />}
+      />
+      <IconButton
+        onPress={deleteSubtask}
+        bg="white"
+        shadow={3}
+        h="100%"
+        w="15%"
+        alignContent="flex-end"
+        icon={<Icon as={MaterialIcons} name="delete" color="primary.500" />}
+      />
     </HStack>
   );
 }
